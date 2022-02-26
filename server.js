d76cd3d5c333ffa4bc3b9235bb551e315065235d
Samuel Maina <mainasamkaranja@gmail.com>
2022-02-26 10:47:05 +0300

    Added express js to serve project

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+
+const server = app.listen(8000, function() {
+  const port = server.address().port;
+  console.log(`Server started at http://localhost:${port}`);
+});
